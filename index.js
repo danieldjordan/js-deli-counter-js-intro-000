2473e67204c0962c5ba6d5f08e15494fc8fd9993
danieldjordan <danieldjordan@gmail.com>
2018-09-21 19:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function takeANumber(deliLine, name){
   deliLine.push(name)
   return (`Welcome, ${name}. You are number ${currentCount+1} in line.`)
 }
+
+function nowServing(deliLine){
+  if(deliLine>0){
+    name = deliLine[deliLine.length-(deliLine.length-1)]
+    deliLine.shift()
+    return name
+  }
+  else{
+    return "There is nobody waiting to be served!"
+  }
+}
